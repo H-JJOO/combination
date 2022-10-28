9872f9aba27d47ade0958e3d0bcd94e991348171
koreait-shopping <icepri3535@gmail.com>
2022-10-28 16:21:11 +0900

    DAY 18 웹개발 종합반 마무리(합본)

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.q15z7ue.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+#홈페이지
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#내 생애 최고의 영화들
+@app.route('/movies')
+def movies():
+    return render_template('movie.html')
+
+@app.route("/movie", methods = ["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers = headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'star':star_receive,
+        'comment':comment_receive
+    }
+
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+@app.route("/movie", methods = ["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies': movie_list})
+
+#화성땅 공동구매
+@app.route('/marsPurchase')
+def mars():
+    return render_template('mars.html')
+
+@app.route("/mars", methods = ["POST"])
+def web_mars_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+
+    doc = {
+        'name':name_receive,
+        'address':address_receive,
+        'size':size_receive
+    }
+    db.mars.insert_one(doc)
+
+    return jsonify({'msg': '주문 완료'})
+
+@app.route("/mars", methods = ["GET"])
+def web_mars_get():
+    order_list = list(db.mars.find({}, {'_id': False}))
+    return jsonify({'orders': order_list})
+
+#팬명록
+@app.route('/fanList')
+def fan():
+   return render_template('fan.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    nickname_receive = request.form['nickname_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'nickname': nickname_receive,
+        'comment': comment_receive
+    }
+
+    db.fanList.insert_one(doc)
+
+    return jsonify({'msg':'응원 등록 완료!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    fan_list = list(db.fanList.find({}, {'_id': False}))
+    return jsonify({'fan_list': fan_list})
+
+#버킷리스트
+@app.route('/bucketList')
+def bucket():
+   return render_template('bucket.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+
+    doc = {
+        'num':count,
+        'bucket':bucket_receive,
+        'done':0
+    }
+
+    db.bucket.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route("/bucket/done", methods=["POST"])
+def bucket_done():
+    num_receive = request.form['num_give']
+    db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '버킷 완료!'})
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'buckets': bucket_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port = 5000, debug = True)
